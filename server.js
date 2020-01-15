b2a9e5a4f527b8c6c4eea983bd9193bdc7c7ca3b
Lexie Jiang <LexieJiang@Lexies-MacBook-Pro.local>
2020-01-15 15:57:03 -0800

    Commit 1: server and index set up

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 
 const server = express();
+const postRouter = require('./posts/postRouter');
+const userRouter = require('./users/userRouter');
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -8,6 +10,11 @@ server.get('/', (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  const { method, originalUrl } = req;
+  console.log(`${method} ${originalUrl} at ${new Date().toISOString()}`);
+  
+  next();
+};
 
 module.exports = server;
