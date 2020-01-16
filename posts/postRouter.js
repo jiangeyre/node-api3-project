2cdf791033d09840bc38e021431b8c1a563187e3
Lexie Jiang <LexieJiang@Lexies-MacBook-Pro.local>
2020-01-15 16:15:13 -0800

    Commit 2: postRouter partially done

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,13 +1,23 @@
 const express = require('express');
 
+const POST = require('./postDb');
+
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  // do your magic!
+  POST.get()
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(err => {
+      res.status(500).json({ error: "The posts could not be retrieved." });
+    })
 });
 
 router.get('/:id', (req, res) => {
-  // do your magic!
+  let id = req.params.id;
+
+  POST.getById
 });
 
 router.delete('/:id', (req, res) => {
@@ -21,7 +31,17 @@ router.put('/:id', (req, res) => {
 // custom middleware
 
 function validatePostId(req, res, next) {
-  // do your magic!
-}
+  let id = req.params.id;
+
+  POST.getById(id)
+    .then(post => {
+      req.post = post;
+
+      next();
+    })
+    .catch(err => {
+      res.status(400).json({ message: "Invalid user ID." });
+    })
+};
 
 module.exports = router;
